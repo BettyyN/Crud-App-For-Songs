422a973e52b9bbfc517b750b5def18b78aee9b19
BettyyN <betelhemnegash10@gmail.com>
2024-05-29 23:51:16 +0300

    unnecessary file removed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from "react";
 import Home from "./Components/home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Edit from "./Components/edit"
-import Delete from "./Components/delete";
-import Update from "./Components/update";
-
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/delete" element={<Delete />} />
-          <Route path="/update" element={<Update />} />
-        </Routes>
-      </Router>
+      <Home/>
     </div>
   );
 }
